14a9e390b328e32206783b44559d169a3f73654c
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-04 22:31:56 -0800

    logging user name log.json and finishing Client constructor

--- lab-pedja/lib/server.js
+++ lab-pedja/lib/server.js
@@ -10,26 +10,32 @@ let logger = new (winston.Logger)({
   ],
 });
 
-logger.log('info','Hello world!');
-// winston.level = 'debug';
-// winston.log('debug','Hello world!');
-// ------------------------------------------------------
-
 const app = net.createServer();
 let clients = [];
-// ------------------------------------------------------
+
+class Client {
+  constructor(socket) {
+    this.id = faker.random.uuid();
+    this.name = faker.internet.userName();
+    this.socket = socket;
+    clients.push(this);
+  }
+};
+
 
 let parseCommand = (message,socket) =>{
-  if(message.startsWith('@')){
+  if(message.startsWith('@')) {
     let parsedCommand = message.split(' ');
     let commandWord = parsedCommand[0];
 
     switch(commandWord){
-    case'@list':// vinicio - if(commandWord === '@list')
+
+    case'@list': // vinicio - if(commandWord === '@list')
       socket.write(clients.map(client => client.name).join('\n') + '\n');
       break;
+
     default:
-      socket.write('Valid commands: @list\n');
+      socket.write(`Valid commands: @list, @quit, @name <new-username>, @dm<to-user-name> <message>\n`);
       break;
     }
     return true;
@@ -38,11 +44,16 @@ let parseCommand = (message,socket) =>{
 };
 
 app.on('connection', (socket) => {
-  socket.name = faker.internet.userName();
-  clients.push(socket);
-  logger.log('info', `Net socket`);// [object object]
+  new Client(socket);
+  let name;
+  clients.forEach(client => {
+    if(client.socket === socket)
+      name = client.name;
+  });
+  logger.log('info', `${name}`);
+
   socket.write('Welcome to 401d19 chatroom\n');
-  socket.write(`Your name is ${socket.name}\n`);
+  socket.write(`Your name is ${name}\n`);
 
 
   socket.on('data',(data) => {
@@ -55,7 +66,7 @@ app.on('connection', (socket) => {
     for(let client of clients){
       //vinicio - instead of doing clients[client] I can use directly client
       if(client !== socket)
-        client.write(`${socket.name}: ${message}\n`);
+        client.write(`${name}: ${message}\n`);
     }
   });
 
@@ -63,7 +74,7 @@ app.on('connection', (socket) => {
     clients = clients.filter((client) => {
       return client !== socket;
     });
-    logger.log('info',`Removing ${socket.name}`);
+    logger.log('info',`Removing ${name}`);
   };
   socket.on('error', removeClient(socket));
   socket.on('close', removeClient(socket));
