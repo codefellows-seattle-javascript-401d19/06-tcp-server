01df25aa2b2b8fc7784335b2a5aeeb02f6647d67
snobeard <fatmattmatt@gmail.com>
2017-12-04 17:20:44 -0800

    @list and @quit functionality working

---
+++ lab-matt/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+// const winston = require('winston');
+
+
+// let logger = new (winston.Logger)({
+//   transports: [
+//     new (winston.transports.Console)(),
+//     new (winston.transports.File)({ filename: 'log.json'}),
+//   ],
+// });
+
+
+// winston.level = 'debug';
+// logger.log('info', 'hello world');
+
+
+const server = require('./lib/server');
+const PORT = process.env.PORT;
+
+server.start(PORT, () => {
+
+});
+
+
+// `events` ------------------------------------------------------------
+// const events = require('events');
+
+// let myEventEmitter = new events. EventEmitter();
+// // mattL - other method, but doesn't work with node
+// // let myEventEmitter = events.create();
+
+// myEventEmitter.on('myEvent', () => {
+//   console.log('I am reacting to an event on line 9');
+// });
+
+// myEventEmitter.on('myEvent', () => {
+//   console.log('I am reacting to an event on line 13');
+// });
+
+
+// myEventEmitter.emit('asdfklajsdf'); // mattL - does nothing since no event is connected to <asdfklajsdf>
+// myEventEmitter.emit('myEvent'); // mattL - connects to, and runs the events listed at line 9 and line 13
---
+++ lab-matt/lib/server.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const net = require('net');
+const faker = require('faker');
+const winston = require('winston');
+
+let logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)(),
+    new (winston.transports.File)({ filename: 'log.json'}),
+  ],
+});
+
+
+winston.level = 'debug';
+logger.log('info', 'hello world');
+
+const app = net.createServer();
+let clients = [];
+
+// ============= PARSE MESSAGE ========================
+
+let parseCommand = (message, socket) => {
+  if (message.startsWith('@')) {
+    let parseCommand = message.split(' ');
+    let commandWord = parseCommand[0];
+    let commandPrompt = parseCommand[1];
+
+    switch (commandWord) {
+      case '@list':
+        socket.write(clients.map(client => client.name).join('\n') + '\n');
+      break;
+
+      case '@quit':
+        clients = clients.filter((client) => {
+          if (client !== socket)
+            return client;
+        });
+        socket.destroy();
+      break;
+
+      case '@nickname':
+        for (let client of clients) {
+          if (!client === socket) {
+            socket.write(' no mo life \n');
+          } else {
+            socket.write(' life \n');
+          }
+        }
+
+      break;
+
+      default:
+        socket.write('valid commands: @list | @quit | @nickname\n');
+      break;
+    }
+    return true;
+  }
+  return false;
+}
+
+// ============== CONNECTION HANDLING ====================
+
+app.on('connection', (socket) => {
+  socket.name = faker.internet.userName();
+  clients.push(socket);
+  logger.log('info', `New Socket`);
+  socket.write('Welcome to 401d19 chatroom\n');
+  socket.write(`Your name is ${socket.name}\n`);
+  
+  socket.on('data', (data) => {
+    logger.log('info', `Processing data: ${data}`);
+    
+    let message = data.toString().trim();
+    
+    removeClient(socket);
+    
+    if (parseCommand(message, socket))
+    return;
+    
+    for(let client of clients) {
+      if (client !== socket) {
+        client.write(`${socket.name}: ${message}\n`);
+      }
+    }
+  });
+
+  let removeClient = (socket) => () => {
+    
+    clients = clients.filter((client) => {
+      return client !== socket;
+    }); 
+
+    logger.log('info', 'removing ${socket.name}');
+  };
+  
+  socket.on('error', () => {
+    removeClient(socket);
+  });
+
+  socket.on('close', () => {
+    removeClient(socket);    
+  });
+});
+
+// ============ SERVER EXPORTS =====================
+
+const server = module.exports = {};
+
+server.start = (port, callback) => {
+  logger.log('info', `Server is up on port ${port}`);
+  console.log('info', `Server is up on port ${port}`);
+  return app.listen(port, callback);
+};
+
+server.stop = (callback) => {
+  logger.log('info', `Server is up off`);
+  return app.close(callback);
+
+};
