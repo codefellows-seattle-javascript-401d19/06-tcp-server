5e91aa7cee6453af9e117c2b9d650b388a98c524
Robert Reed <robert.mc.reed@gmail.com>
2017-12-04 18:43:38 -0800

    server accepts connections and can send text

--- lab-rob/lib/server.js
+++ lab-rob/lib/server.js
@@ -6,6 +6,7 @@ const faker = require('faker');
 const app = net.createServer();
 
 let clients = [];
+const chatRoomName = faker.company.catchPhrase();
 
 class Client {
   constructor(socket) {
@@ -41,7 +42,37 @@ let logger = new (winston.Logger)({
 });
 
 app.on('connection', socket => {
-  new Client(socket);
+  let currentClient = new Client(socket);
+  logger.log('info', `New client connected! Name: ${currentClient.name}, id: ${currentClient.id}`);
+  socket.write(`Welcome to the ${chatRoomName} chat room!\n`);
+
+  socket.on('data', data => {
+    logger.log('info', `${currentClient.name} typed: ${data}`);
+    for(let client of clients) {
+      if(client.socket !== socket)
+        client.socket.write(`${client.name}: ${data}`);
+    }
+    // put other client logic here, parsedCommands?
+  });
+
+  let removeClient = socket => () => {
+    clients = clients.filter(client => {
+      return client.socket !== socket;
+    });
+  };
+
+  socket.on('error', removeClient(socket));
+  socket.on('close', removeClient(socket));
 });
 
+const server = module.exports = {};
+server.start = (port, callback) => {
+  logger.log('info', `Server is running on port ${port}.`);
+  console.log(`Server is running on port ${port}.`);
+  return app.listen(port, callback);
+};
 
+server.stop = callback => {
+  logger.log('info', 'Server shutting down.');
+  return app.close(callback);
+};
